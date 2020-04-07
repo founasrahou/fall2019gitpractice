c4c967e6da2dd7f3ed670e9316650e08c2675f36
founasrahou <founasrahou@hotmail.com>
2020-04-07 15:14:01 -0700

    added a line

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,7 +1,7 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hello world again");
-//        System.out.println("Hello world again");
+        System.out.println("Hello world again");
 //        System.out.println("Hello world again");
 //        System.out.println("Hello world again");
 //        System.out.println("Hello world again");
